83e52dec04be0105f167ec7330578d309f8ed725
matheuskya <kenzoyamashitaasano@gmail.com>
2025-06-04 15:58:42 -0300

    estrutura atualizada

---
+++ Farmacia/src/Negocio.java
@@ -0,0 +1,76 @@
+package negocio;
+
+import java.util.UUID;
+
+//id utilizando uuid
+public class Negocio {
+    private UUID id;
+    private TipoNegocio tipo;
+    private List<ItemNegocio> itens;
+    private List<Funcionario> funcionariosEnvolvidos;
+    private StatusNegocio status;
+    private double valorTotalNegocio;
+
+    public Negocio(UUID id, TipoNegocio tipo, List<ItemNegocio> itens, List<Funcionario> funcionariosEnvolvidos, StatusNegocio status, double valorTotalNegocio) {
+        this.id = id;
+        this.tipo = tipo;
+        this.itens = itens;
+        this.funcionariosEnvolvidos = funcionariosEnvolvidos;
+        this.status = status;
+        this.valorTotalNegocio = valorTotalNegocio;
+    }
+
+    public Double calcularValorTotal(){
+        return 0.0;
+    }
+
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public TipoNegocio getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoNegocio tipo) {
+        this.tipo = tipo;
+    }
+
+    public List<ItemNegocio> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemNegocio> itens) {
+        this.itens = itens;
+    }
+
+    public List<Funcionario> getFuncionariosEnvolvidos() {
+        return funcionariosEnvolvidos;
+    }
+
+    public void setFuncionariosEnvolvidos(List<Funcionario> funcionariosEnvolvidos) {
+        this.funcionariosEnvolvidos = funcionariosEnvolvidos;
+    }
+
+    public StatusNegocio getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusNegocio status) {
+        this.status = status;
+    }
+
+    public double getValorTotalNegocio() {
+        return valorTotalNegocio;
+    }
+
+    public void setValorTotalNegocio(double valorTotalNegocio) {
+        this.valorTotalNegocio = valorTotalNegocio;
+    }
+}
